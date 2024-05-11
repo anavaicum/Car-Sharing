4cc93a5f5a7175493549880dc5fd6954cd99548e
VoinaRares <66557261+VoinaRares@users.noreply.github.com>
2024-05-11 13:08:10 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Proiect_Colectiv.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Proiect_Colectiv.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Facultate/OOP/OOP Proiect Colectiv/Google_tests/lib
-# Build directory: C:/Facultate/OOP/OOP Proiect Colectiv/cmake-build-debug/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
