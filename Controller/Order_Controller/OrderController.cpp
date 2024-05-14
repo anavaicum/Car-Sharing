c6a977b92cbc382d22449a52936677fc0426d03e
aron396 <bandi.aron2003@gmail.com>
2024-05-14 21:16:44 +0300

    Added Comments to OrderController.cpp

--- Controller/Order_Controller/OrderController.cpp
+++ Controller/Order_Controller/OrderController.cpp
@@ -1,8 +1,11 @@
-#include "OrderController.h"
-
-Order_Controller::Order_Controller(const std::string& type) : type(type) {}
-
-bool Order_Controller::create_order(const Order& order) {
-    // Dummy implementation, always return true
-    return true;
-}
+//#include "OrderController.h"
+//
+//Order_Controller::Order_Controller(const std::string& type) : type(type) {}
+//
+//bool Order_Controller::create_order(const Order& order) {
+//    // Here, you would typically add the order to a database or some other storage mechanism
+//    // For the sake of this example, let's assume we have a vector to store orders
+//    // and we just push the new order into that vector
+//    orders.push_back(order);
+//    return true;
+//}
