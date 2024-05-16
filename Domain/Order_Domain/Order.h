d18465fa41d0b4a4310eb13f1579108039490554
Cosmin <cosminserban2003@gmail.com>
2024-05-16 13:39:20 +0300

    added "TODO" for employee

--- Domain/Order_Domain/Order.h
+++ Domain/Order_Domain/Order.h
@@ -7,40 +7,6 @@
 #include <vector>
 using namespace std;
 
-class Date {
-private:
-
-    int day;
-    int month;
-    int year;
-
-public:
-
-    Date(int day, int month, int year);
-
-    int getDay() const;
-
-    int getMonth() const;
-
-    int getYear() const;
-
-    void setDay(int day);
-
-    void setMonth(int month);
-
-    void setYear(int year);
-
-    bool operator<(const Date &rhs) const;
-
-    bool operator>(const Date &rhs) const;
-
-    bool operator<=(const Date &rhs) const;
-
-    bool operator>=(const Date &rhs) const;
-
-
-};
-
 class Order {
 private:
 
