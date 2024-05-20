b7a2147149d18516ddc2f4e5e5dc85deb9a9ec93
AlexiaMariaV <alexiavoina2004@gmail.com>
2024-05-20 23:56:46 +0300

    Added create_worker prototype

--- UI/AdminUI.cpp
+++ UI/AdminUI.cpp
@@ -4,9 +4,39 @@
 #include <iostream>
 using namespace std;
 #include "AdminUI.h"
-void AdminUI::createWorker() {
+void AdminUI::create_worker() {
+    int id;
+    string first_name, last_name, email, password, position, initials;
+    float salary;
+    int day, month, year;
+    Date birthday (day, month, year);
+    bool is_Admin;
+
+    cout <<"Worker's id: ";
+    cin >> id;
+    cout <<"Worker's first name: ";
+    cin >> first_name;
+    cout <<"Worker's last name: ";
+    cin >> last_name;
+    cout <<"Worker's email: ";
+    cin >> email;
+    cout <<"Worker's password: ";
+    cin >> password;
+    cout <<"Worker's position: ";
+    cin >> position;
+    cout <<"Worker's initials: ";
+    cin >> initials;
+    cout <<"Worker's salary: ";
+    cin >> salary;
+    cout <<"Worker's birthday: ";
+    cin >> day;
+    cin >> month;
+    cin >> year;
+    //birthday(day, month, year);
+    cout <<"Is Worker Admin?: ";
+    cin >> is_Admin;
+
 }
-void updateWorker() {}
 void activateWorker() {}
 void deactivateWorker() {}
 void deleteWorker() {}
