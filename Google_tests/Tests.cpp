9c44db455e25e869723cc1c4810eab7081786c24
VoinaRares <voinarares14@gmail.com>
2024-05-24 16:42:55 +0300

    Changed to_CSV to not save directly to the file and instead return a string

--- Google_tests/Tests.cpp
+++ Google_tests/Tests.cpp
@@ -13,31 +13,23 @@ TEST(TestGtest, FirstTest) {
 TEST(SavingData, CarSaving){
     Car c("SB12OGV", "Sandero", "Dacia", 2017, 100000.5, 12,
           Car::Gas, Car::Manual, "Red", vector<string>());
+    Car c2;
+    c2 = c2.From_String_To_Object(c.to_CSV());
 
-    c.save_to_CSV("../../Google_tests/TestRepos/CarRepoTest.txt");
+    ASSERT_EQ(c2.getLicensePlate(), c.getLicensePlate());
+    ASSERT_EQ(c2.getModel(), c.getModel());
+    ASSERT_EQ(c2.getBrand(), c.getBrand());
+    ASSERT_EQ(c2.getYearOfFirstReg(), c.getYearOfFirstReg());
+    ASSERT_EQ(c2.getMileage(), c.getMileage());
+    ASSERT_EQ(c2.getPricePerDay(), c.getPricePerDay());
+    ASSERT_EQ(c2.getFuel(), c.getFuel());
+    ASSERT_EQ(c2.getTrans(), c.getTrans());
+    ASSERT_EQ(c2.getColor(), c.getColor());
+    ASSERT_EQ(c2.getRemarks().size(), c.getRemarks().size());
 
-    ifstream file("../../Google_tests/TestRepos/CarRepoTest.txt");
-    string line;
-    getline(file, line); // Removing Header
-
-    getline(file, line);
-
-    c = c.From_String_To_Object(line);
-    ASSERT_EQ(c.getLicensePlate(), "SB12OGV");
-    ASSERT_EQ(c.getModel(), "Sandero");
-    ASSERT_EQ(c.getBrand(), "Dacia");
-    ASSERT_EQ(c.getYearOfFirstReg(), 2017);
-    //ASSERT_EQ(c.getMileage(), 100000.5);
-    ASSERT_EQ(c.getPricePerDay(), 12);
-    ASSERT_EQ(c.getFuel(), Car::Gas);
-    ASSERT_EQ(c.getTrans(), Car::Manual);
-    ASSERT_EQ(c.getColor(), "Red");
-    for(int i = 0; i < c.getRemarks().size(); i++){
-        ASSERT_EQ(true, false);
-    }
 }
 
-TEST(SavingData, CustomerSaving){
+TEST(SavingData, CustomerSaving) {
     vector<string> rm;
     rm.push_back("primul");
     rm.push_back("second");
@@ -48,26 +40,7 @@ TEST(SavingData, CustomerSaving){
     fav.push_back(c);
     Customer cus(1, "this", "password", "Gica", "Popescu", "1234",
                  "Str. Lui", false, fav);
-    cus.save_to_CSV("../../Google_tests/TestRepos/CustomerRepoTest.txt");
 
-    ifstream file("../../Google_tests/TestRepos/CustomerRepoTest.txt");
-    string line;
-
-    getline(file, line); //Removing Header
-
-    getline(file, line);
-
-    cus = Customer().From_String_To_Object(line);
-
-    ASSERT_EQ(cus.get_id(), 1);
-    ASSERT_EQ(cus.get_email(), "this");
-    ASSERT_EQ(cus.get_password(), "password");
-    ASSERT_EQ(cus.get_first_name(), "Gica");
-    ASSERT_EQ(cus.get_last_name(), "Popescu");
-    ASSERT_EQ(cus.get_phone(), "1234");
-    ASSERT_EQ(cus.get_address(), "Str. Lui");
-    ASSERT_EQ(cus.is_GDPRdeleted(), false);
-    ASSERT_EQ(cus.get_favorites()[0].getRemarks()[0], "primul");
 }
 
 
