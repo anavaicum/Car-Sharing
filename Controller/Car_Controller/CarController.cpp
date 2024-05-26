6df521a35c4800f7595e232c902e6be37da311e7
aron396 <bandi.aron2003@gmail.com>
2024-05-26 19:42:27 +0300

    CarController connected to Repo

--- Controller/Car_Controller/CarController.cpp
+++ Controller/Car_Controller/CarController.cpp
@@ -1,32 +1,30 @@
-// Car_Controller.cpp
-#include "CarController.h"
+#include "../../Controller/Car_Controller/CarController.h"
+#include <algorithm> // Include this header for std::sort
 
-// Constructor
-Car_Controller::Car_Controller(const string& type, shared_ptr<IRepo<Car>> repo) : type(type), car_repo(repo) {
-    this->car_repo->read_from_file();
+Car_Controller::Car_Controller(const std::shared_ptr<IRepo<Car>> &repo) : carRepo(repo) {
+    load_cars();
 }
 
-// Function to add a car to the controller
-void Car_Controller::addCar(const Car& car) {
-    this->car_repo->add(car);
-}
-
-// Function to search for a car by license plate
-Car Car_Controller::search_by_license_plate(const string& license_plate) const {
-    for (const auto& car : car_repo->get_all()) { //
+Car Car_Controller::search_by_license_plate(const std::string &license_plate) const {
+    std::vector<Car> cars = carRepo->get_all();
+    for (const auto &car : cars) {
         if (car.getLicensePlate() == license_plate) {
             return car;
         }
     }
-//    // Return a default-constructed Car if not found
-//    return Car("", "", "", 0, 0.0, 0.0, "", {}); // TO DO: this is an invalid constructor, replaced it with: throw exception()
-
-    throw exception(); // if object not found
+    throw std::runtime_error("Car not found");
 }
 
-// Function to get ordered cars for a specific customer
-vector<Car> Car_Controller::get_ordered_cars(const string& customer_id) const {
-    // Dummy implementation, returning an empty vector
-    return vector<Car>();
+std::vector<Car> Car_Controller::get_ordered_cars(int customer_id) const {
+    std::vector<Car> cars = carRepo->get_all();
+    std::sort(cars.begin(), cars.end(), [](const Car &a, const Car &b) {
+        return a.getPricePerDay() < b.getPricePerDay();
+    });
+    return cars;
+}
+
+
+void Car_Controller::load_cars() {
+    carRepo->read_from_file();
 }
 
